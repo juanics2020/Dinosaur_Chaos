1dcd4e704f98c3863bb234c25ba6a356ddfe15a4
1DAW1920 <1DAW1920@DESKTOP-4TE9TSB>
2020-01-28 14:53:30 +0100

    Dinosaurio dia 3

--- src/main/java/es/juanics/dinosaur_chaos/App.java
+++ src/main/java/es/juanics/dinosaur_chaos/App.java
@@ -31,7 +31,19 @@ public class App extends Application {
     Image dinosaur7 = new Image(getClass().getResourceAsStream("/images/Idle (7).png"));    
     Image dinosaur8 = new Image(getClass().getResourceAsStream("/images/Idle (8).png"));
     Image dinosaur9 = new Image(getClass().getResourceAsStream("/images/Idle (9).png"));
-    Image dinosaur10 = new Image(getClass().getResourceAsStream("/images/Idle (10).png"));    
+    Image dinosaur10 = new Image(getClass().getResourceAsStream("/images/Idle (10).png"));
+    
+    Image dinosaur11 = new Image(getClass().getResourceAsStream("/images/Idleiz (1).png"));
+    Image dinosaur12 = new Image(getClass().getResourceAsStream("/images/Idleiz (2).png"));
+    Image dinosaur13 = new Image(getClass().getResourceAsStream("/images/Idleiz (3).png"));
+    Image dinosaur14 = new Image(getClass().getResourceAsStream("/images/Idleiz (4).png"));
+    Image dinosaur15 = new Image(getClass().getResourceAsStream("/images/Idleiz (5).png"));
+    Image dinosaur16 = new Image(getClass().getResourceAsStream("/images/Idleiz (6).png"));
+    Image dinosaur17 = new Image(getClass().getResourceAsStream("/images/Idleiz (7).png"));    
+    Image dinosaur18 = new Image(getClass().getResourceAsStream("/images/Idleiz (8).png"));
+    Image dinosaur19 = new Image(getClass().getResourceAsStream("/images/Idleiz (9).png"));
+    Image dinosaur20 = new Image(getClass().getResourceAsStream("/images/Idleiz (10).png"));     
+    
     ImageView dinosaurView1 = new ImageView(dinosaur1);//meter la imagen dinosaurio en view
      
     int i = 1;//para timeline imagen dinosaur
@@ -78,106 +90,135 @@ public class App extends Application {
         //PONER EL DINOSAURIO
         dinosaurView1.setX((SCENE_WIDTH/2)-(TAMAÑO_DINO*4));//posición x del dinosaurio
         dinosaurView1.setY(SCENE_HEIGHT-(SCENE_HEIGHT/3));//posición y del dinosaurio
-        dinosaurView1.setFitHeight ((SCENE_HEIGHT*TAMAÑO_DINO)/100);//El dinosario será el 15% de la pantalla
-        dinosaurView1.setFitWidth ((SCENE_WIDTH*TAMAÑO_DINO)/100);         
+        dinosaurView1.setFitHeight ((SCENE_HEIGHT*dinosaurView1.getFitHeight())/100);//El dinosario será el 15% de la pantalla
+        dinosaurView1.setFitWidth ((SCENE_WIDTH*dinosaurView1.getFitWidth())/100);         
         root.getChildren().add(dinosaurView1);
-        
-        
-            //
-            scene.setOnKeyPressed(new EventHandler<KeyEvent>(){//Llama al método setOnKeyPressed. Cuando detecte que se pulsa una tecla en la escena (se puede hacer que en vez que en la escena se detecte cuando pulse dentro de un campo de texto)
-                public void handle(final KeyEvent keyEvent){
-                    posicionMomView1 = backgroundView1.getX(); // Cuando pulsemos una tecla leeremos la posición actual del visor y después moveremos la imagen
-                    posicionMomView2 = backgroundView2.getX(); // Cuando pulsemos una tecla leeremos la posición actual del visor y después moveremos la imagen          
-                    double limite = (SCENE_WIDTH/2)-(TAMAÑO_DINO*2);//Límite a donde llega el dinosaurio y comienza scroll
-                    
-                    switch(keyEvent.getCode()){//Según la tecla pulsada
-                        case LEFT:// el dinosaurio se moverá a la izquierda
-                            direction = -1; //pantalla a la izquierda
-                            if (dinosaurView1.getX()>0){
-                                dinosaurView1.setX ((dinosaurView1.getX())-8);
-                            }
-                            break;
- 
-                        case RIGHT: //el fondo se moverá a la izquierda
-                            direction = 1;//pantalla a la derecha
-                            posicionMomView1 = posicionMomView1-8;
-                            posicionMomView2 = posicionMomView2-8;
-                            if (dinosaurView1.getX()>limite){
-                                if (posicionMomView2>0){
-                                    backgroundView1.setX (posicionMomView1);
-                                    backgroundView2.setX (posicionMomView2);
-                                }else{
-                                    backgroundView1.setX (0);
-                                    backgroundView2.setX (SCENE_WIDTH);                                
-                                }
-                            }else{
-                                dinosaurView1.setX ((dinosaurView1.getX())+8);                               
-                            }
-                            break;
-                            
-                        case UP:
-                            direction = 0;//pantalla parada
-                            System.out.println("up");
-                            break;
-                    }
+                
+        //
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>(){//Llama al método setOnKeyPressed. Cuando detecte que se pulsa una tecla en la escena (se puede hacer que en vez que en la escena se detecte cuando pulse dentro de un campo de texto)
+            public void handle(final KeyEvent keyEvent){
+                posicionMomView1 = backgroundView1.getX(); // Cuando pulsemos una tecla leeremos la posición actual del visor y después moveremos la imagen
+                posicionMomView2 = backgroundView2.getX(); // Cuando pulsemos una tecla leeremos la posición actual del visor y después moveremos la imagen          
+                double limite = (SCENE_WIDTH/2)-(TAMAÑO_DINO*2);//Límite a donde llega el dinosaurio y comienza scroll
 
+                switch(keyEvent.getCode()){//Según la tecla pulsada
+                    case LEFT:// el dinosaurio se moverá a la izquierda
+                        direction = -1; //pantalla a la izquierda
+                        i = 11;//cambair contador imagen IDLE izquierda
+                        if (dinosaurView1.getX()>0){
+                            dinosaurView1.setX ((dinosaurView1.getX())-8);
+                        }
+                        break;
+
+                    case RIGHT: //el fondo se moverá a la izquierda
+                        direction = 1;//pantalla a la derecha
+                         i = 1;//cambiar contador imagen IDLE
+                        posicionMomView1 = posicionMomView1-8;
+                        posicionMomView2 = posicionMomView2-8;
+                        if (dinosaurView1.getX()>limite){
+                            if (posicionMomView2>0){
+                                backgroundView1.setX (posicionMomView1);
+                                backgroundView2.setX (posicionMomView2);
+                            }else{
+                                backgroundView1.setX (0);
+                                backgroundView2.setX (SCENE_WIDTH);                                
+                            }
+                        }else{
+                            dinosaurView1.setX ((dinosaurView1.getX())+8);                               
+                        }
+                        break;
+
+                    case UP:
+                        direction = 0;//pantalla parada
+                        System.out.println("up");
+                        break;
                 }
-            });        
-            //
-   
+
+            }
+        });        
+        //
+        //TIMELINE -IDLE
         // Game loop usando Timeline
         Timeline timeline = new Timeline(//Sirve para lo que lo que metamos aquí. Podemos utilizar varios TimeLine con diferentes velocidades para diferentes cosas
             // 0.017 ~= 60 FPS (equivalencia de segundos a Frames por Segundo)
-            new KeyFrame(Duration.seconds(0.017), new EventHandler<ActionEvent>() {
+            new KeyFrame(Duration.seconds(0.08), new EventHandler<ActionEvent>() {
                 public void handle(ActionEvent ae) {//Sólo puede haber un handle en el timeline
                    //SWITCH PARA QUE LAS IMÀGENES DEL DINOSAURIO CAMBIEN
-
-                    switch (i){
-                        case 1:
-                            dinosaurView1 = new ImageView (dinosaur1);//meter la imagen dinosaurio en view                           
-                            i++;
-                        case 2:
-                            dinosaurView1 = new ImageView (dinosaur2);//meter la imagen dinosaurio en view
-                            i++;
-                        case 3:
-                            dinosaurView1 = new ImageView (dinosaur3);//meter la imagen dinosaurio en view           
-                            i++;
-                        case 4:
-                            dinosaurView1 = new ImageView (dinosaur4);//meter la imagen dinosaurio en view           
-                            i++;
-                        case 5:
-                            dinosaurView1 = new ImageView (dinosaur5);//meter la imagen dinosaurio en view                               
-                            i++;
-                        case 6:
-                            dinosaurView1 = new ImageView (dinosaur6);//meter la imagen dinosaurio en view                                    
-                            i++;
-                        case 7:
-                            dinosaurView1 = new ImageView (dinosaur7);//meter la imagen dinosaurio en view                                    
-                            i++;
-                        case 8:
-                            dinosaurView1 = new ImageView (dinosaur8);//meter la imagen dinosaurio en view                                       
-                            i++;
-                        case 9:
-                            dinosaurView1 = new ImageView (dinosaur9);//meter la imagen dinosaurio en view                                                               
-                            i++;
-                        case 10:
-                            dinosaurView1 = new ImageView (dinosaur10);//meter la imagen dinosaurio en view                                           
-                            i = 0; 
+                    if (direction == 1){
+                        switch (i){
+                            case 1:
+                                dinosaurView1.setImage(dinosaur1);//meter la imagen dinosaurio en view                           
+                                break;
+                            case 2:
+                                dinosaurView1.setImage(dinosaur2);//meter la imagen dinosaurio en view
+                                break;
+                            case 3:
+                                dinosaurView1.setImage(dinosaur3);//meter la imagen dinosaurio en view           
+                                break;                        
+                            case 4:
+                                dinosaurView1.setImage(dinosaur4);//meter la imagen dinosaurio en view           
+                                break;                             
+                            case 5:
+                                dinosaurView1.setImage(dinosaur5);//meter la imagen dinosaurio en view                               
+                                break;                        
+                            case 6:
+                                dinosaurView1.setImage(dinosaur6);//meter la imagen dinosaurio en view                                    
+                                break;                             
+                            case 7:
+                                dinosaurView1.setImage(dinosaur7);//meter la imagen dinosaurio en view                                    
+                                break;                        
+                            case 8:
+                                dinosaurView1.setImage(dinosaur8);//meter la imagen dinosaurio en view                                       
+                                break;                        
+                            case 9:
+                                dinosaurView1.setImage(dinosaur9);//meter la imagen dinosaurio en view                                                               
+                                break;                        
+                            case 10:
+                                dinosaurView1.setImage(dinosaur10);//meter la imagen dinosaurio en view                                           
+                                i = 0;
+                                break;
+                        }
+                    }else if (direction == -1){
+                        switch (i){
+                            case 11:
+                                dinosaurView1.setImage(dinosaur11);//meter la imagen dinosaurio en view                           
+                                break;
+                            case 12:
+                                dinosaurView1.setImage(dinosaur12);//meter la imagen dinosaurio en view
+                                break;
+                            case 13:
+                                dinosaurView1.setImage(dinosaur13);//meter la imagen dinosaurio en view           
+                                break;                        
+                            case 14:
+                                dinosaurView1.setImage(dinosaur14);//meter la imagen dinosaurio en view           
+                                break;                             
+                            case 15:
+                                dinosaurView1.setImage(dinosaur15);//meter la imagen dinosaurio en view                               
+                                break;                        
+                            case 16:
+                                dinosaurView1.setImage(dinosaur16);//meter la imagen dinosaurio en view                                    
+                                break;                             
+                            case 17:
+                                dinosaurView1.setImage(dinosaur17);//meter la imagen dinosaurio en view                                    
+                                break;                        
+                            case 18:
+                                dinosaurView1.setImage(dinosaur18);//meter la imagen dinosaurio en view                                       
+                                break;                        
+                            case 19:
+                                dinosaurView1.setImage(dinosaur19);//meter la imagen dinosaurio en view                                                               
+                                break;                        
+                            case 20:
+                                dinosaurView1.setImage(dinosaur20);//meter la imagen dinosaurio en view                                           
+                                i = 10;
+                                break;
+                        }                        
                     }
-                    
-                    
-                    /*
-                    //idle();//la imagen del dinosaurio irá cambiando
-                    if (i<=10){//Hasta que la i no llegue a 10 irá cambiando el dinosaurio
-                        dinosaurView1.setImage("/images/Idle ("+ i +").png");//********
-                        i++;
-                    }else {//cuando la i llegue a 11, la volvemos a poner a 1
-                        i=1;    
-                    }*/
+                    i++;
                 }
             })                
         );
-        
+        timeline.setCycleCount(Timeline.INDEFINITE);//Llama al método setCycleCount (para que la animación siga indefinidamente
+        timeline.play(); //Llama al método Play para echar a andar la animación
            
     }
 
